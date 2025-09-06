2c78869555bd734380e9790f80c0144d39532ea1
howandt <howandt@gmail.com>
2025-09-06 14:16:50 +0200

    Fix utils.ts and resolve button import error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
-export function cn(...classes: (string | undefined | null | false)[]) {
-  return classes.filter(Boolean).join(" ")
+export function cn(...inputs: (string | undefined | null | false)[]) {
+  return inputs.filter(Boolean).join(" ");
 }
+
